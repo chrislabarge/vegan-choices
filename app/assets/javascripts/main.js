9613d5bf18c974040c72c12251a04dc9d922cc2a
chrislabarge <chrislabarge2@gmail.com>
2017-02-12 18:50:41 -0500

    beef up logic of displaying the content modal

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     .modal( {inverted: true })
     .modal('setting', 'transition', 'fade down')
   ;
+
   $('.message .close')
     .on('click', function() {
       $(this)
@@ -10,7 +11,19 @@ $(document).ready(function(){
         .transition('fade')
       ;
   });
+
+
+  $('.toggle-ingredients').click(function() {
+    window.remoteRequestButton = $(this);
+
+    initializeLoader();
+  });
 });
+
+function initializeLoader() {
+  $('body').append("<div class='ui active inverted dimmer'><div class='ui large text loader'>Loading</div></div><p></p><p></p><p></p>");
+  $('body').addClass('dimmed')
+}
 
 function toggleNested (parent, childClass) {
   $(parent)
--- spec/features/restaurants/items/item_ingredients/show_spec.rb
+++ spec/features/restaurants/items/item_ingredients/show_spec.rb
@@ -13,8 +13,32 @@ feature 'Show: Ingredients' do
     they_should_be_shown_the_item_ingredients(item_ingredients)
   end
 
+  scenario 'View Item Ingredients twice', js: true do
+    item = create_restaurant_item_with_ingredients
+    restaurant = item.restaurant
+    item_ingredients = item.main_item_ingredients
+
+    given_a_vistor_is_viewing_a(:restaurant, restaurant)
+
+    when_they_toggle_the_ingredients_the_button_changes(item_ingredients)
+  end
+
+  def when_they_toggle_the_ingredients_the_button_changes(item_ingredients)
+    when_they_click_show_ingredients_for_an_item
+
+    they_should_be_shown_the_item_ingredients(item_ingredients)
+
+    when_they_close_the_ingredient_modal
+
+    when_they_click_modal_toggle_ingredients_for_an_item
+  end
+
+  def the_show_ingredient_button_should_change
+    expect(page.find_link('Show Ingredients').visible?).not_to eq true
+    expect(page.find_button('Show Ingredients').visible?).to eq true
+  end
+
   def they_should_be_shown_the_item_ingredients(item_ingredients)
-    # Sleep for controller to process.  Throws race error without.
     sleep(1)
 
     item_ingredients.each do |item_ingredient|
@@ -31,5 +55,13 @@ feature 'Show: Ingredients' do
 
   def when_they_click_show_ingredients_for_an_item
     click_link('Show Ingredients')
+  end
+
+  def when_they_click_modal_toggle_ingredients_for_an_item
+    click_button('Show Ingredients')
+  end
+
+  def when_they_close_the_ingredient_modal
+    find(:css, '.close').click
   end
 end
