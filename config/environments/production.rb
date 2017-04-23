094f5a998b2a92f17b7d371c60a2eabc59643031
chrislabarge <chrislabarge2@gmail.com>
2017-04-23 15:42:03 -0400

    add secrets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # In replace of secret.yml
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
