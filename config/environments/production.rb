f8b166ae6d76152c650238f562f00c1d40574566
chrislabarge <chrislabarge2@gmail.com>
2017-06-27 21:42:26 -0400

    fix mailer config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: ENV['HOST_NAME'] }
-  config.action_mailer.delivery_method = :smtp.settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     address:  'smtp.sendgrid.net',
     port:     '587',
     authentication:  :plain,
