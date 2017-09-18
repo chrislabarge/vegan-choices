fd93da11601eae18035c7e59ccc31a0c69023684
chrislabarge <chrislabarge2@gmail.com>
2017-09-18 10:52:20 -0400

    add omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :omniauthable,
+         omniauth_providers: [:twitter, :google_oauth2, :facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      info = auth.info
+
+      user.email = info.email
+      puts auth.info.inspect
+      user.password = Devise.friendly_token[0,20]
+      user.name = info.nickname   # assuming the user model has a name
+      # user.image = info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
