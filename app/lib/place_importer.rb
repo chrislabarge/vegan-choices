67c4e19c8d3f8b11d30755f207f3d50f8c9f744b
chrislabarge <chrislabarge2@gmail.com>
2018-02-19 11:31:52 -0500

    add new importer method

--- app/lib/place_importer.rb
+++ app/lib/place_importer.rb
@@ -11,7 +11,6 @@ class PlaceImporter
                  end
   end
 
-
   def import_places(locations = nil)
     locations ||= @locations
     locations.each { |location| import_place(location) }
@@ -28,19 +27,49 @@ class PlaceImporter
 
   def import_google_place(place_id, location = nil)
     place = @client.spot(place_id)
-    location ||= Location.create
     restaurant = Restaurant.create(name: place.name, website: place.website, photo_url: place.photos[0].try(:fetch_url, 800))
+    return unless restaurant.persisted?
 
-    location.update_attributes( restaurant: restaurant,
-                                country: place.country,
-                                state: place.region,
-                                city: place.city,
-                                latitude: place.lat,
-                                longitude: place.lng,
-                                street: place.street,
-                                street_number: place.street_number,
-                                phone_number: place.international_phone_number,
-                                hours: place.opening_hours.try(:to_json))
+    location ||= Location.create
 
+    location.update_attributes(restaurant: restaurant,
+                               country: place.country,
+                               state: place.region,
+                               city: place.city,
+                               latitude: place.lat,
+                               longitude: place.lng,
+                               street: place.street,
+                               street_number: place.street_number,
+                               phone_number: place.international_phone_number,
+                               hours: place.opening_hours.try(:to_json))
+
+  end
+
+  def validate_places(namesnil)
+    pass = []
+    fail = []
+
+    names.each do |name|
+      results = @client.predictions_by_input(name)
+
+      if results.empty?
+        fail << name
+        next
+      end
+
+      puts results.first.inspect
+
+      pass << name
+    end
+
+    puts "Passing: #{pass}"
+    puts "Failing: #{fail}"
+  end
+
+  def import_by_names(names)
+    names.each do |name|
+      results = @client.predictions_by_input(name)
+      import_google_place results.first.place_id
+    end
   end
 end
